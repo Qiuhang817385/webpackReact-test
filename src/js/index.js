8979c05e0098dfd6742eb114f951cff2ccc410eb
Qiuhang817385 <826266129@qq.com>
2020-06-09 20:47:38 +0800

    stage1

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Link from './link';
-import './../css/style.scss';
-ReactDom.render(
-  <Link />,
-  document.getElementById('div1')
-)
